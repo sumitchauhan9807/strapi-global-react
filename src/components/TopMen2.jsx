ed17da9fa1da84e18644c69ea2b0a76169a8a8ee
Sumit chauhan <sumitchauhan9807666@gmail.com>
2024-11-02 11:58:56 +0530

    wip

--- src/components/TopMen2.jsx
+++ src/components/TopMen2.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect,useRef } from "react"
-import { productMenuItems } from "./menuItems"
+import { productMenuItems ,solutionsMenuItems ,pricingMenuItems} from "./menuItems"
+import LanguageSelect from 'src/components/small/LanguageSelector'
 
 export const ProductMenuList = ({ref}) => {
     return (
@@ -19,64 +20,84 @@ export const ProductMenuList = ({ref}) => {
     )
 }
 
+export const PricingMenuList = ({ref}) => {
+    return (
+        <div className="flex flex-col gap-8 absolute left-[-10rem] top-[90%] bg-[#24282e] w-[500px] h-[700px] overflow-hidden z-20 p-8 rounded-2xl" ref={ref}>
+            {pricingMenuItems.map(item => (
+                <div className="flex gap-4 text-white">
+                    <figure className="flex justify-center items-center p-3 bg-[#141b24] text-white rounded-md">
+                        {item.icon}
+                    </figure>
+                    <div className="flex flex-col text-[#e3e0e0]">
+                        <strong className="text-base">{item.title}</strong>
+                        <small className="text-sm">{item.description}</small>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export const SolutionMenuList = ({ref}) => {
+    return (
+        <div className="flex flex-col gap-8 absolute left-[-10rem] top-[90%] bg-[#24282e] w-[500px] h-[300px] overflow-hidden z-20 p-8 rounded-2xl" ref={ref}>
+            {solutionsMenuItems.map(item => (
+                <div className="flex gap-4 text-white">
+                    <figure className="flex justify-center items-center p-3 bg-[#141b24] text-white rounded-md">
+                        {item.icon}
+                    </figure>
+                    <div className="flex flex-col text-[#e3e0e0]">
+                        <strong className="text-base">{item.title}</strong>
+                        <small className="text-sm">{item.description}</small>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
 
 export default function TopMen2() {
     const ref = useRef(null);
     const [activeMenu, setActiveMenu] = useState('')
-    useEffect(() => {
-        const handleOutSideClick = (event) => {
-            if (!ref.current?.contains(event.target)) {
-                setActiveMenu('')
-            }
-        };
-        ['mousedown'].forEach(event => window.addEventListener(event, handleOutSideClick));
-        return () => {
-            ['mousedown'].forEach(event => window.addEventListener(event, handleOutSideClick));
-        };
-    }, [ref])
-    return (
+	return (
         <menu className="flex items-center justify-between p-2 px-8 h-24">
             <figure className="h-12 w-12 border-2 border-black rounded-full mx-8">
                 <embed src="" type="" />
             </figure>
             <div className="flex text-xl text-[#3b516d] relative items-center right-16">
-                <div className="relative p-4">
+                <div className="relative p-4 cursor-pointer" onMouseLeave={()=>{ setActiveMenu('')}} onMouseEnter={()=>{ setActiveMenu('products')}} >
                     <span
                         className={`uppercase font-semibold rounded-t-md p-4 px-6 ${activeMenu === 'products' ? 'bg-[#24282e] text-white' : 'text-[#3b516d]'}`}
-                        onClick={() => setActiveMenu('products')}
                     >
                         Products
                     </span>
                     {activeMenu === 'products' && <ProductMenuList ref={ref}/>}
                 </div>
-                <div className="relative p-4">
+                <div className="relative p-4 cursor-pointer" onMouseLeave={()=>{ setActiveMenu('')}} onMouseEnter={()=>{ setActiveMenu('features')}}>
                     <span
                         className={`uppercase font-semibold rounded-t-md p-4 px-6 ${activeMenu === 'features' ? 'bg-[#24282e] text-white' : 'text-[#3b516d]'}`}
-                        onClick={() => setActiveMenu('features')}
                     >
-                        Features
+                        Solutions
                     </span>
-                    {activeMenu === 'features' && <ProductMenuList />}
+                    {activeMenu === 'features' && <SolutionMenuList />}
                 </div>
-                <div className="relative p-4">
+                <div className="relative p-4 cursor-pointer" onMouseLeave={()=>{ setActiveMenu('')}} onMouseEnter={()=>{ setActiveMenu('pricing')}}>
                     <span
                         className={`uppercase font-semibold rounded-t-md p-4 px-6 ${activeMenu === 'pricing' ? 'bg-[#24282e] text-white' : 'text-[#3b516d]'}`}
-                        onClick={() => setActiveMenu('pricing')}
                     >
                         Pricing
                     </span>
-                    {activeMenu === 'pricing' && <ProductMenuList />}
+                    {activeMenu === 'pricing' && <PricingMenuList />}
                 </div>
-                <div className="relative p-4">
+                <div className="relative p-4 cursor-pointer">
                     <span
                         className="uppercase font-semibold px-6"
                     >
                         About
                     </span>
                 </div>
-                <select name="" id="" className="border p-2 rounded">
-                    <option value="">Language</option>
-                </select>
+                <LanguageSelect/>
             </div>
         </menu>
     )
--- src/components/menuItems.jsx
+++ src/components/menuItems.jsx
@@ -9,27 +9,90 @@ import { TbSettingsAutomation } from "react-icons/tb";
 export const productMenuItems = [
     {
         icon: <GrAnalytics size={28}/>,
-        title: 'Analytics',
-        description: 'Get better understanding of traffic'
+        title: 'USA Phone Numbers',
+        description: 'Virtual phone numbers from KOMPaaS for VoIP telephony (SIP)'
     },
     {
         icon: <LuMousePointerClick size={28}/>,
-        title: 'Engagement',
-        description: 'Speak directly to your customers'
+        title: 'SIP Trunk',
+        description: "SIP Trunk is a virtual communication channel that connects the operator's telecommunications platform (CPaaS) to the customer's equipment"
     },
     {
         icon: <GrShieldSecurity size={28}/>,
-        title: 'Security',
-        description: `Your customers' data will be safe and secure`
+        title: 'Automated calls',
+        description: `Software product which automates call center together with Virtual PBX services`
     },
     {
         icon: <GrIntegration size={28}/>,
-        title: 'Integrations',
-        description: `Connect with third-party tools`
+        title: 'Call Tracking',
+        description: `Call-tracking without extra charge directly form the telecom operator`
     },
     {
         icon: <TbSettingsAutomation size={28}/>,
-        title: 'Automations',
-        description: `Build strategic funnels that will convert`
+        title: 'Multichat',
+        description: `Full chat service for customers with free callback feature`
     },
+]
+
+export const solutionsMenuItems = [
+    {
+        icon: <GrAnalytics size={28}/>,
+        title: 'Phone Numbers',
+        description: 'Phone numbers for corporate customers'
+    },
+    {
+        icon: <LuMousePointerClick size={28}/>,
+        title: 'Virtual PBX',
+        description: "Robot controller,Home Office,Call Recording"
+    },
+    {
+        icon: <GrShieldSecurity size={28}/>,
+        title: 'Automated calls',
+        description: `Automatic calls for online pharmacies`
+    }
+]
+
+export const pricingMenuItems = [
+    {
+        icon: <GrAnalytics size={28}/>,
+        title: 'Virtual PBX',
+        description: 'Virtual PBX Pricing'
+    },
+		{
+			icon: <GrAnalytics size={28}/>,
+			title: 'USA Phone Numbers',
+			description: 'USA Phone Numbers'
+		},
+		{
+			icon: <GrAnalytics size={28}/>,
+			title: 'Foreign Phone Numbers',
+			description: 'Foreign Phone Numbers'
+		},
+		{
+			icon: <GrAnalytics size={28}/>,
+			title: 'SIP Trunk',
+			description: 'SIP Trunk Pricing'
+		},
+		{
+			icon: <GrAnalytics size={28}/>,
+			title: 'Multichat',
+			description: 'Multichat Pricing'
+		},
+		{
+			icon: <GrAnalytics size={28}/>,
+			title: 'Automated Calls',
+			description: 'Automated Calls Pricing'
+		},
+		{
+			icon: <GrAnalytics size={28}/>,
+			title: 'Chatbot',
+			description: 'Chatbot Pricing'
+		},
+		{
+			icon: <GrAnalytics size={28}/>,
+			title: 'Integrations',
+			description: 'Integrations Pricing'
+		},
+
+
 ]
