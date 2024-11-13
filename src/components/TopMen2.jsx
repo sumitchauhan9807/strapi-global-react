de80cd535c209dc3a39f8dd2ce4fc3430bba858e
Sumit chauhan <sumitchauhan9807666@gmail.com>
2024-11-13 11:25:07 +0530

    wip

--- src/components/TopMen2.jsx
+++ src/components/TopMen2.jsx
@@ -1,13 +1,10 @@
-import { useState, useEffect, useRef } from "react"
-import { productMenuItems, solutionsMenuItems, pricingMenuItems } from "./menuItems"
+import { useState, useEffect,useRef } from "react"
+import { productMenuItems ,solutionsMenuItems ,pricingMenuItems} from "./menuItems"
 import LanguageSelect from 'src/components/small/LanguageSelector'
-import Logo from 'src/assets/logo.png'
-import { NavLink } from "react-router-dom"
-import {SolutionMenuList} from "./menuItems"
 
-export const ProductMenuList = ({ ref }) => {
+export const ProductMenuList = ({ref}) => {
     return (
-        <div className="flex flex-col gap-8 absolute left-[-10rem] top-[90%] bg-[#24282e] w-[500px] h-[500px] overflow-hidden z-20 p-8 rounded-2xl">
+        <div className="flex flex-col gap-8 absolute left-[-10rem] top-[90%] bg-[#24282e] w-[500px] h-[500px] overflow-hidden z-20 p-8 rounded-2xl" ref={ref}>
             {productMenuItems.map(item => (
                 <div className="flex gap-4 text-white">
                     <figure className="flex justify-center items-center p-3 bg-[#141b24] text-white rounded-md">
@@ -23,10 +20,28 @@ export const ProductMenuList = ({ ref }) => {
     )
 }
 
-export const PricingMenuList = ({ ref }) => {
+export const PricingMenuList = ({ref}) => {
     return (
-        <div className="flex flex-col gap-8 absolute left-[-10rem] top-[90%] bg-[#24282e] w-[500px] h-[700px] overflow-hidden z-20 p-8 rounded-2xl">
+        <div className="flex flex-col gap-8 absolute left-[-10rem] top-[90%] bg-[#24282e] w-[500px] h-[700px] overflow-hidden z-20 p-8 rounded-2xl" ref={ref}>
             {pricingMenuItems.map(item => (
+                <div className="flex gap-4 text-white">
+                    <figure className="flex justify-center items-center p-3 bg-[#141b24] text-white rounded-md">
+                        {item.icon}
+                    </figure>
+                    <div className="flex flex-col text-[#e3e0e0]">
+                        <strong className="text-base">{item.title}</strong>
+                        <small className="text-sm">{item.description}</small>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export const SolutionMenuList = ({ref}) => {
+    return (
+        <div className="flex flex-col gap-8 absolute left-[-10rem] top-[90%] bg-[#24282e] w-[500px] h-[300px] overflow-hidden z-20 p-8 rounded-2xl" ref={ref}>
+            {solutionsMenuItems.map(item => (
                 <div className="flex gap-4 text-white">
                     <figure className="flex justify-center items-center p-3 bg-[#141b24] text-white rounded-md">
                         {item.icon}
@@ -44,41 +59,30 @@ export const PricingMenuList = ({ ref }) => {
 
 export default function TopMen2() {
     const ref = useRef(null);
-    const [activeMenu, setActiveMenu] = useState('');
-    useEffect(() => {
-        const handleOutSideClick = (event) => {
-            if (!ref.current?.contains(event.target)) {
-                setActiveMenu('')
-            }
-        };
-        window.addEventListener('mousedown', handleOutSideClick);
-        return () => {
-            window.addEventListener('mousedown', handleOutSideClick);
-        };
-    }, [ref]);
-
-
-    return (
-        <menu className="flex items-center justify-between p-2 px-8 h-24" ref={ref}>
-            <img style={{ height: "97px" }} src={Logo} />
+    const [activeMenu, setActiveMenu] = useState('')
+	return (
+        <menu className="flex items-center justify-between p-2 px-8 h-24">
+            <figure className="h-12 w-12 border-2 border-black rounded-full mx-8">
+                <embed src="" type="" />
+            </figure>
             <div className="flex text-xl text-[#3b516d] relative items-center right-16">
-                <div className="relative p-4 cursor-pointer" onClick={() => setActiveMenu('products')}>
+                <div className="relative p-4 cursor-pointer" onMouseLeave={()=>{ setActiveMenu('')}} onMouseEnter={()=>{ setActiveMenu('products')}} >
                     <span
                         className={`uppercase font-semibold rounded-t-md p-4 px-6 ${activeMenu === 'products' ? 'bg-[#24282e] text-white' : 'text-[#3b516d]'}`}
                     >
                         Products
                     </span>
-                    {activeMenu === 'products' && <ProductMenuList />}
+                    {activeMenu === 'products' && <ProductMenuList ref={ref}/>}
                 </div>
-                <div className="relative p-4 cursor-pointer" onClick={() => setActiveMenu('solutions')}>
+                <div className="relative p-4 cursor-pointer" onMouseLeave={()=>{ setActiveMenu('')}} onMouseEnter={()=>{ setActiveMenu('features')}}>
                     <span
-                        className={`uppercase font-semibold rounded-t-md p-4 px-6 ${activeMenu === 'solutions' ? 'bg-[#24282e] text-white' : 'text-[#3b516d]'}`}
+                        className={`uppercase font-semibold rounded-t-md p-4 px-6 ${activeMenu === 'features' ? 'bg-[#24282e] text-white' : 'text-[#3b516d]'}`}
                     >
                         Solutions
                     </span>
-                    {activeMenu === 'solutions' && <SolutionMenuList />}
+                    {activeMenu === 'features' && <SolutionMenuList />}
                 </div>
-                <div className="relative p-4 cursor-pointer" onClick={() => setActiveMenu('pricing')}>
+                <div className="relative p-4 cursor-pointer" onMouseLeave={()=>{ setActiveMenu('')}} onMouseEnter={()=>{ setActiveMenu('pricing')}}>
                     <span
                         className={`uppercase font-semibold rounded-t-md p-4 px-6 ${activeMenu === 'pricing' ? 'bg-[#24282e] text-white' : 'text-[#3b516d]'}`}
                     >
@@ -93,7 +97,7 @@ export default function TopMen2() {
                         About
                     </span>
                 </div>
-                <LanguageSelect />
+                <LanguageSelect/>
             </div>
         </menu>
     )
