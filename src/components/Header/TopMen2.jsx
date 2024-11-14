c9c8c97e575dd5ccef5d5e1b8425d0df0eb9f745
Nilshan <nilshanalaguraj6@gmail.com>
2024-11-14 16:10:43 +0530

    update

--- src/components/Header/TopMen2.jsx
+++ src/components/Header/TopMen2.jsx
@@ -1,15 +1,15 @@
-import { useState, useEffect,useRef } from "react"
-import { productMenuItems ,solutionsMenuItems ,pricingMenuItems} from "../menuItems"
+import { useState, useEffect, useRef } from "react"
+import { productMenuItems, solutionsMenuItems, pricingMenuItems } from "../menuItems"
 import LanguageSelect from 'src/components/small/LanguageSelector'
 import { Link } from "react-router-dom"
 import Logo from 'src/assets/logo.png'
-import {  useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { TbSettingsAutomation } from "react-icons/tb";
 
 export const ProductMenuList = () => {
     return (
         <div className="flex flex-col gap-8 absolute left-[-10rem] top-[90%] bg-[#24282e] w-[500px] h-[690px] overflow-hidden z-20 p-8 rounded-2xl">
-            {productMenuItems.map((item,index) => (
+            {productMenuItems.map((item, index) => (
                 <Link key={index} to={item.link} className="flex gap-4 text-white">
                     <figure className="flex justify-center items-center p-3 bg-[#141b24] text-white rounded-md">
                         {item.icon}
@@ -45,7 +45,7 @@ export const PricingMenuList = () => {
 export const SolutionMenuList = () => {
     return (
         <div className="flex flex-col gap-8 absolute left-[-10rem] top-[90%] bg-[#24282e] w-[500px] h-[990px] overflow-hidden z-20 p-8 rounded-2xl">
-            {solutionsMenuItems.map((item,index) => (
+            {solutionsMenuItems.map((item, index) => (
                 <Link key={index} to={item.link} className="flex gap-4 text-white">
                     <figure className="flex justify-center items-center p-3 bg-[#141b24] text-white rounded-md">
                         {item.icon}
@@ -61,79 +61,96 @@ export const SolutionMenuList = () => {
 }
 
 export const SolutionMenuList2 = () => {
-	return (
-		<div id="mega-menu-full-dropdown" className=" absolute left-[-10rem] top-[90%] bg-[#24282e] w-[500px] overflow-hidden z-20 p-8 rounded-2xl ">
-  <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
-    <ul>
-      <li>
-        <a href="#" className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-				<figure className="flex justify-center items-center p-3 bg-[#141b24] text-white rounded-md">
-                        <TbSettingsAutomation/>
-                    </figure>
-          <div className="font-semibold">Online Stores</div>
-          <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-        </a>
-      </li>
-      <li>
-        <a href="#" className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-          <div className="font-semibold">Segmentation</div>
-          <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-        </a>
-      </li>
-      <li>
-        <a href="#" className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-          <div className="font-semibold">Marketing CRM</div>
-          <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-        </a>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <a href="#" className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-          <div className="font-semibold">Online Stores</div>
-          <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-        </a>
-      </li>
-      <li>
-        <a href="#" className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-          <div className="font-semibold">Segmentation</div>
-          <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-        </a>
-      </li>
-      <li>
-        <a href="#" className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-          <div className="font-semibold">Marketing CRM</div>
-          <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-        </a>
-      </li>
-    </ul>
-  </div>
-</div>
+    return (
+        <div id="mega-menu-full-dropdown" className=" absolute left-[-10rem] top-[90%] bg-[#24282e] w-[700px] overflow-hidden z-20 p-8 rounded-2xl ">
+            <div className="grid max-w-screen-2xl px-2 py-2 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-2">
+                <ul>
+                    <li>
+                        <a href="#" className="flex items-center p-1 rounded-lg hover:bg-gray-100 gap-2 dark:hover:bg-gray-700">
 
-	)
+                            <figure className="flex justify-center items-center w-12 h-12 bg-[#141b24] text-white rounded-md">
+                                <TbSettingsAutomation />
+                            </figure>
+
+                            <div>
+                                <p className="font-semibold text-sm">Online Stores</p>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">
+                                    Connect with third-party tools that you're already using.
+                                </span>
+                            </div>
+                        </a>
+
+                    </li>
+                    <li>
+                        <a href="#" className="flex items-center p-1 rounded-lg hover:bg-gray-100 gap-2 dark:hover:bg-gray-700">
+                            <figure className="flex justify-center items-center w-12 h-12 bg-[#141b24] text-white rounded-md">
+                                <TbSettingsAutomation />
+                            </figure><div className="font-semibold">Segmentation                        <br />     <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="flex items-center p-1 rounded-lg hover:bg-gray-100 gap-2 dark:hover:bg-gray-700">
+                        <figure className="flex justify-center items-center w-12 h-12 bg-[#141b24] text-white rounded-md">
+                                <TbSettingsAutomation />
+                            </figure> <div className="font-semibold">Marketing CRM <br />                            <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="#" className="flex items-center p-1 rounded-lg hover:bg-gray-100 gap-2 dark:hover:bg-gray-700">
+                        <figure className="flex justify-center items-center w-12 h-12 bg-[#141b24] text-white rounded-md">
+                                <TbSettingsAutomation />
+                            </figure> <div className="font-semibold">Online Stores <br />                            <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="flex items-center p-1 rounded-lg hover:bg-gray-100 gap-2 dark:hover:bg-gray-700">
+                        <figure className="flex justify-center items-center w-12 h-12 bg-[#141b24] text-white rounded-md">
+                                <TbSettingsAutomation />
+                            </figure> <div className="font-semibold">Segmentation <br />                            <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="flex items-center p-1 rounded-lg hover:bg-gray-100 gap-2 dark:hover:bg-gray-700">
+                        <figure className="flex justify-center items-center w-12 h-12 bg-[#141b24] text-white rounded-md">
+                                <TbSettingsAutomation />
+                            </figure> <div className="font-semibold">Marketing CRM <br />                            <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    )
 }
 
 
 export default function TopMen2() {
     const ref = useRef(null);
     const [activeMenu, setActiveMenu] = useState('')
-		let location = useLocation()
-		useEffect(()=>{
-			setActiveMenu('')
-		},[location])
-	return (
+    let location = useLocation()
+    useEffect(() => {
+        setActiveMenu('')
+    }, [location])
+    return (
         <menu className="flex items-center justify-between p-2 px-8 h-24">
-           <Link to="/"><img src={Logo} style={{height:"98px"}}/></Link>
+            <Link to="/"><img src={Logo} style={{ height: "98px" }} /></Link>
             <div className="flex text-[#3b516d] relative items-center right-16">
-                <div style={{fontSize:"15px"}} className="relative p-4 cursor-pointer" onMouseLeave={()=>{ setActiveMenu('')}} onMouseEnter={()=>{ setActiveMenu('products')}} >
+                <div style={{ fontSize: "15px" }} className="relative p-4 cursor-pointer" onMouseLeave={() => { setActiveMenu('') }} onMouseEnter={() => { setActiveMenu('products') }} >
                     <span
                         className={`uppercase font-semibold rounded-t-md p-4 px-6 ${activeMenu === 'products' ? 'bg-[#24282e] text-white' : 'text-[#3b516d]'}`}
                     >
                         Products
                     </span>
-                    {activeMenu === 'products' && <ProductMenuList/>}
+                    {activeMenu === 'products' && <ProductMenuList />}
                 </div>
-                <div style={{fontSize:"15px"}} className="relative p-4 cursor-pointer" onMouseLeave={()=>{ setActiveMenu('')}} onMouseEnter={()=>{ setActiveMenu('features')}}>
+                <div style={{ fontSize: "15px" }} className="relative p-4 cursor-pointer" onMouseLeave={() => { setActiveMenu('') }} onMouseEnter={() => { setActiveMenu('features') }}>
                     <span
                         className={`uppercase font-semibold rounded-t-md p-4 px-6 ${activeMenu === 'features' ? 'bg-[#24282e] text-white' : 'text-[#3b516d]'}`}
                     >
@@ -149,21 +166,21 @@ export default function TopMen2() {
                     </span>
                     {activeMenu === 'pricing' && <PricingMenuList />}
                 </div> */}
-								<Link style={{fontSize:"15px"}} to="/pricing" className="relative p-4 cursor-pointer">
+                <Link style={{ fontSize: "15px" }} to="/pricing" className="relative p-4 cursor-pointer">
                     <span
                         className="uppercase font-semibold px-6"
                     >
                         Pricing
                     </span>
                 </Link>
-                <div style={{fontSize:"15px"}} className="relative p-4 cursor-pointer">
+                <div style={{ fontSize: "15px" }} className="relative p-4 cursor-pointer">
                     <span
                         className="uppercase font-semibold px-6"
                     >
                         About
                     </span>
                 </div>
-                <LanguageSelect/>
+                <LanguageSelect />
             </div>
         </menu>
     )
@@ -171,5 +188,5 @@ export default function TopMen2() {
 
 // add logo
 // fotter things
-// adding data to Backend 
+// adding data to Backend
 // adding data in all langs
