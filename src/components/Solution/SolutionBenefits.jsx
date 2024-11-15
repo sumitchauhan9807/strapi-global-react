aa440aa2239f97d36444b8a70e2a7b34c6711ce2
Sumit chauhan <sumitchauhan9807666@gmail.com>
2024-11-15 22:53:12 +0530

    sd

--- src/components/Solution/SolutionBenefits.jsx
+++ src/components/Solution/SolutionBenefits.jsx
@@ -1,7 +1,8 @@
 function SolutionBenefits({ data }) {
+	if(!data.length) return
 	return (
 		<section className="flex flex-col max-w-[1200px] items-center mx-auto gap-8">
-			{data.length && <h4 className="text-xl font-bold my-16">Important benefits</h4> }
+			<h4 className="text-xl font-bold my-16">Important benefits</h4>
 			<div className="flex gap-8">
 				{data.map((benefit) => {
 					return (
