da829543d9f935d7eee0621991e60db4da51f459
Sumit chauhan <sumitchauhan9807666@gmail.com>
2024-11-19 17:31:21 +0530

    wip

--- src/components/Solution/SolutionDescription.jsx
+++ src/components/Solution/SolutionDescription.jsx
@@ -6,7 +6,7 @@ function SolutionDescription({data}) {
 				<small>SOLUTION</small>
 				<span className="text-3xl font-semibold my-4">{data.Heading}</span>
 				<p className="text-base font-light">{data.SubHeading}</p>
-				<button className="w-max p-2 px-4 rounded text-white bg-[#141b24] my-8">BUY NOW</button>
+				{/* <button className="w-max p-2 px-4 rounded text-white bg-[#141b24] my-8">BUY NOW</button> */}
 			</div>
 		</section>
 	);
