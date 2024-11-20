e06b290d7fca029db1061dc48a06cd52da536076
Nilshan <nilshanalaguraj6@gmail.com>
2024-11-20 19:59:30 +0530

    update

--- src/components/PricingTabs.jsx
+++ src/components/PricingTabs.jsx
@@ -6,7 +6,7 @@ import { PageSkeleton } from "src/components/small/Skeletons";
 import Animate from "src/components/Basic/Animate";
 import { baseUrl } from "src/helpers";
 
-let qs = constructQueryString(["Pricing", "Pricing.Plans", "Pricing.Plans.Features", "Pricing.Plans.MissingFeatures","TopImage"]);
+let qs = constructQueryString(["Pricing", "Pricing.Plans", "Pricing.Plans.Features", "Pricing.Plans.MissingFeatures", "TopImage"]);
 export const PricingPage = () => {
 	const language = useSelector((state) => state.language);
 
@@ -23,14 +23,15 @@ export const PricingPage = () => {
 	if (loading) return <PageSkeleton />;
 	if (!data?.data?.Pricing) return;
 	let pricingData = data?.data?.Pricing;
-//https://cdn.sanity.io/images/yk5gp8um/prod/2bfdb6e79211d931344d574cf773b94dd4690b20-3600x2160.jpg?w=1280&h=720&auto=format
+	//https://cdn.sanity.io/images/yk5gp8um/prod/2bfdb6e79211d931344d574cf773b94dd4690b20-3600x2160.jpg?w=1280&h=720&auto=format
 	return (
 		<>
 			<div>
 				<div className="min-h-full bg-gray-50 pb-12">
-					<div style={{backgroundImage:`url(${
-                baseUrl() + data?.data.TopImage.url
-            })`,backgroundPosition:"center"}} className="w-full bg-blue-900 pt-16 pb-24 text-center">
+					<div style={{
+						backgroundImage: `url(${baseUrl() + data?.data.TopImage.url
+							})`, backgroundPosition: "center"
+					}} className="w-full bg-blue-900 pt-16 pb-24 text-center">
 						<h4 className="text-2xl text-gray-100"> {pricingData.Heading}</h4>
 						<p className="text-sm text-gray-100 mt-2">{pricingData.SubText}</p>
 					</div>
@@ -73,43 +74,27 @@ let PricingTab = ({ pricing }) => {
 				{pricing.Features.map((feature, index) => (
 					<li key={index} className="flex items-center space-x-2 px-8">
 						<span className="rounded-full p-1">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-3 w-3"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path
-									fillRule="evenodd"
-									d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-									clipRule="evenodd"
-								/>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="green" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-6">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 							</svg>
+
 						</span>
 						<span>{feature.text}</span>
 					</li>
 				))}
 				{pricing.MissingFeatures.map((feature, index) => (
-					<li key={index} className="flex items-center space-x-2 px-8">
+					<li key={index} className="flex items-center space-x-2 px-8 line-through gray-300">
 						<span className="rounded-full p-1">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-3 w-3"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path
-									fillRule="evenodd"
-									d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-									clipRule="evenodd"
-								/>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-6">
+								<path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 							</svg>
+
 						</span>
 						<span>{feature.text}</span>
 					</li>
 				))}
 			</ul>
-			
+
 		</div>
 	);
 };
