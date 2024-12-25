85cc4e1195491208ae4aa196779f52c7b9735b81
Sumit chauhan <sumitchauhan9807666@gmail.com>
2024-12-25 22:35:07 +0530

    wipo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import store from "src/redux/store";
 import { Provider } from "react-redux";
-
+import './i18';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
