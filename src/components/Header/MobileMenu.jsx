59140832ad8d71d2329584f3a2c85d0599804c68
Sumit chauhan <sumitchauhan9807666@gmail.com>
2024-12-25 23:56:23 +0530

    wip

--- src/components/Header/MobileMenu.jsx
+++ src/components/Header/MobileMenu.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from 'src/assets/logo.png'
-import { productMenuItems ,solutionsMenuItems ,pricingMenuItems} from "src/components/menuItems"
+import { useProductMenuItems ,useSolutionsMenuItems } from "../menuItems";
 import {  useLocation } from 'react-router-dom'
 import { GlobalData } from "src/context";
 import { useContext } from 'react';
@@ -94,6 +94,7 @@ function MobileMenu() {
 }
 
 const ProductMenuList = () => {
+	let productMenuItems = useProductMenuItems()
   return (
     <div id="mega-menu-full-dropdown" className="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
     <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
@@ -113,6 +114,7 @@ const ProductMenuList = () => {
 }
 
 const SolutionMenuList = () => {
+	let solutionsMenuItems = useSolutionsMenuItems()
   return (
     <div id="mega-menu-full-dropdown" className="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
     <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,5 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from 'i18next-browser-languagedetector';
 import en from 'src/translations/en.json'
 import de from 'src/translations/de.json'
 // the translations
