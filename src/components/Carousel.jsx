20edc737fbb8ba264c2158fab9ef1d892c50ac5e
Sumit chauhan <sumitchauhan9807666@gmail.com>
2024-12-30 01:02:27 +0530

    wip

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,6 +6,7 @@ import { explodeArray, baseUrl } from "src/helpers";
 import { useWindowDimensions } from "src/Hooks/HelperHooks";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import 'src/assets/css/slick-custom.css'
 // const ItemsPerRow = 2;
 // const Columns = 4;
 
@@ -34,7 +35,7 @@ export default function SimpleSlider({ countries }) {
 		// ),
 		customPaging: function(i) {
       return (
-        <a>
+        <a className=" mt-4 flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
           {i+1}
         </a>
       );
