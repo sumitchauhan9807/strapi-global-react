fb292e3663c0a27b0bd06d7417c7c233bf77823e
Sumit chauhan <sumitchauhan9807666@gmail.com>
2024-12-30 16:21:47 +0530

    wip

--- src/components/Product/ProductDescription.jsx
+++ src/components/Product/ProductDescription.jsx
@@ -3,6 +3,38 @@ import Animate from "src/components/Basic/Animate";
 
 function ProductDescription({ data }) {
 	return (
+		<div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+			<div className="grid gap-10 lg:grid-cols-2 items-center">
+				<Animate
+					config={{
+						animateIn: "bounceInLeft",
+					}}
+				>
+					<img src={baseUrl() + data.Image.url} alt="Robot" className="" />
+				</Animate>
+				<Animate
+					config={{
+						animateIn: "bounceInRight",
+					}}
+				>
+					<h3 className="text-blue-500 uppercase font-bold text-sm">Product</h3>
+					<h1 className="text-5xl font-semi-bold text-gray-800 mt-2 mb-4">{data.ProductName}</h1>
+					<p className="text-gray-600 text-lg mb-4 font-sans ">{data.ProductDescription}</p>
+					<ul className="list-none space-y-2 text-gray-700 grid lg:grid-cols-2 grid-cols-1 font-sans ">
+						{data.ProductSpecs.map((list) => {
+							return <li key={list.id}>{list.text}</li>;
+						})}
+					</ul>
+					<div className="flex items-center gap-6 mt-6 ">
+						<div className="border-2 shadow-2xl  px-6 py-4 rounded-lg gap-3">
+							<span className="text-xl font-sans uppercase text-gray-800">{data.ProductPrice}</span> <br />
+						</div>
+					</div>
+				</Animate>
+			</div>
+		</div>
+	);
+	return (
 		<>
 			<div className="flex items-center justify-center">
 				<div className="flex-1 ">
@@ -11,7 +43,7 @@ function ProductDescription({ data }) {
 							animateIn: "bounceInLeft",
 						}}
 					>
-						<img src={baseUrl() + data.Image.url} alt="Robot" style={{maxWidth:"500px"}} className="" />
+						<img src={baseUrl() + data.Image.url} alt="Robot" style={{ maxWidth: "500px" }} className="" />
 					</Animate>
 				</div>
 				<div className="flex-2 p-6">
@@ -31,9 +63,6 @@ function ProductDescription({ data }) {
 						<div className="flex items-center gap-6 mt-6 ">
 							<div className="border-2 shadow-2xl  px-6 py-4 rounded-lg gap-3">
 								<span className="text-xl font-sans uppercase text-gray-800">{data.ProductPrice}</span> <br />
-								{/* <center><button className="bg-gray-600 hover:bg-gray-500 pr-5 text-white py-2 px-4 rounded transition duration-300 mt-2">
-                  BUY NOW
-                </button></center> */}
 							</div>
 						</div>
 					</Animate>
--- src/components/Solution/SolutionCTA.jsx
+++ src/components/Solution/SolutionCTA.jsx
@@ -1,13 +1,31 @@
-import { baseUrl } from 'src/helpers'
+import { baseUrl } from "src/helpers";
+import Animate from "src/components/Basic/Animate";
 
-function SolutionCTA({data}) {
+function SolutionCTA({ data }) {
+	return (
+		<div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+			<div className="grid gap-10 lg:grid-cols-2 items-center">
+				<Animate
+					config={{
+						animateIn: "bounceInLeft",
+					}}
+				>
+					<img src={baseUrl() + data.Image.url} style={{minWidth:"400px"}} />
+				</Animate>
+				<Animate
+					config={{
+						animateIn: "bounceInRight",
+					}}
+				>
+					<h3 className="text-3xl font-semibold my-4">{data.Heading}</h3>
+					<p className="text-base font-light my-4">{data.Description}</p>
+				</Animate>
+			</div>
+		</div>
+	);
 	return (
 		<section className="grid grid-cols-2 gap-8 my-16 max-w-[1280px] mx-auto bg-[#fafafa] p-8">
-			<img src={baseUrl() + data.Image.url} style={{minWidth:"400px"}}/>
-			<div className="flex flex-col">
-				<h3 className="text-3xl font-semibold my-4">{data.Heading}</h3>
-				<p className="text-base font-light my-4">{data.Description}</p>
-			</div>
+			<div className="flex flex-col"></div>
 		</section>
 	);
 }
